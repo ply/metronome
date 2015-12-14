261b3b6263a4a6871acc5346b837ef9d84334b2c
jgrochal <hand.over.cookies@gmail.com>
2015-12-14 07:16:06 +0100

    simple tests

--- tests/src/com/example/metronome/TickerTest.java
+++ tests/src/com/example/metronome/TickerTest.java
@@ -2,15 +2,19 @@ package com.example.metronome;
 
 import android.test.AndroidTestCase;
 
+import android.content.Context;
+
 /**
- * Tests for MetronomeBackend
+ * Tests for Ticker
  */
 public class TickerTest extends AndroidTestCase {
 
-    MetronomeBackend backend;
+    Ticker ticker;
+    Context context;
+    DotsSurfaceView dots;
 
     public void setUp() throws Exception {
-        backend = new MetronomeBackend();
+        ticker = new Ticker(context, dots);
     }
 
     public void tearDown() throws Exception {
@@ -18,19 +22,49 @@ public class TickerTest extends AndroidTestCase {
     }
 
     public void testRun() throws Exception {
-        assertFalse(backend.isRunning());
-        backend.start();
-        assertTrue(backend.isRunning());
-        backend.start();
-        assertTrue(backend.isRunning());
-        backend.stop();
-        assertFalse(backend.isRunning());
-        backend.stop();
-        assertFalse(backend.isRunning());
-        backend.start();
-        assertTrue(backend.isRunning());
-        backend.stop();
-        assertFalse(backend.isRunning());
+        assertFalse(ticker.isRunning());
+        ticker.start();
+        assertTrue(ticker.isRunning());
+        ticker.start();
+        assertTrue(ticker.isRunning());
+        ticker.stop();
+        assertFalse(ticker.isRunning());
+        ticker.stop();
+        assertFalse(ticker.isRunning());
+        ticker.start();
+        assertTrue(ticker.isRunning());
+        ticker.stop();
+        assertFalse(ticker.isRunning());
     }
 
+    public void testIsAudible() throws Exception {
+        assertFalse(ticker.isAudible());
+        ticker.enableSound();
+        assertTrue(ticker.isAudible());
+        ticker.disableSound();
+        assertFalse(ticker.isAudible());
+
+    }
+
+    public void testSetBPM(){
+        assertTrue(ticker.getBPM() == 2);
+        ticker.setBPM(5);
+        assertFalse(ticker.getBPM() == 2);
+        ticker.setBPM(6);
+        assertTrue(ticker.getBPM() == 6);
+        ticker.setBPM(5);
+        assertFalse(ticker.getBPM() == 6);
+        ticker.setBPM(2);
+        assertTrue(ticker.getBPM() == 2);
+    }
+
+    public void testSetMeasure(){
+        assertTrue(ticker.getMeasure() == 120);
+        ticker.setMeasure(60);
+        assertFalse(ticker.getMeasure() == 120);
+        ticker.setMeasure(300);
+        assertFalse(ticker.getMeasure() == 60);
+        ticker.setMeasure(300);
+        assertTrue(ticker.getMeasure() == 300);
+    }
 }
